e144ae6ca76978635eb2767d37aafc002b8b5b82
lixiaopeng06 <lixiaopeng06.baidu.com>
2022-11-29 16:17:20 +0800

    星光

--- src/pages/Threejs/Particles/index.jsx
+++ src/pages/Threejs/Particles/index.jsx
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 const Particles = () => {
-    return <div id="particles" className="case" />;
+    return <div id="particles" className="case">这里是星光</div>;
 };
 
 export default Particles;
